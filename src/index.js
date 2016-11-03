80d531b4aad4a221420241e01f6be9a26a88bf94
HONG ZHOU <holly_zhou@yahoo.com>
2016-11-02 22:42:53 -0700

    react-router

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store from './store';
 import Layout from './components/Layout';
+// import HomePage from './components/HomePage';
+// import ThreadPage from './components/ThreadPage';
 
 render(
   <Provider store={store}>
-    <Layout />
+    <Router history={browserHistory}>
+      <Route path="/" component={Layout}>
+        {/* <IndexRoute component={HomePage} />
+        <Route path="thread" component={ThreadPage} /> */}
+      </Route>
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
