e01385dbb5eeba03f2858bcbc17a7b72549ea393
HONG ZHOU <holly_zhou@yahoo.com>
2016-11-02 23:34:48 -0700

    add navbar and layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,11 +1,23 @@
-import React, { Component } from 'react';
+import React, { PropTypes } from 'react';
+import { Menu, Segment } from 'semantic-ui-react';
+import { Link } from 'react-router';
 
-export default class Layout extends Component {
-  render() {
-    return (
-      <div className='container'>
-        <h1 className='text-center'>React Webpack</h1>
-      </div>
-    )
-  }
+export default function Layout(props) {
+  return (
+    <div>
+      <Segment inverted>
+        <Menu inverted secondary className="navBar">
+          <Menu.Item header>CH-Chan</Menu.Item>
+          <Menu.Menu position="right">
+            <Link to="/"><Menu.Item name="home" active="home" /></Link>
+          </Menu.Menu>
+        </Menu>
+      </Segment>
+      {props.children}
+    </div>
+  );
 }
+
+Layout.PropTypes = {
+  clildren: PropTypes.object, //eslint-disable-line
+};
